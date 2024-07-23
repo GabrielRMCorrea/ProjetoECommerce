a7e6130805d437f9127b0100b64b14bc32a30c18
GabrielRMCorrea <35786887+GabrielRMCorrea@users.noreply.github.com>
2024-07-22 21:24:08 -0400

    first

---
+++ src/category/category-repository.ts
@@ -0,0 +1,29 @@
+import pisma from '../prisma-client'
+
+const getOne = async  () => {
+    
+};
+
+const getAll = async () => {
+
+};
+
+const create = async () =>{
+
+};
+
+const update = async () =>{
+
+};
+
+const destroy = async () =>{
+
+};
+
+export default {
+    getOne,
+    getAll,
+    create,
+    update,
+    destroy
+}
